e953c4f1e527105533fb055639205e835473d0d1
Chetan Gupta <chetangupta2708@gmail.com>
2024-01-14 18:38:59 +0530

    Cors aded

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 
 app.use(express.json())
 const corsOptions = {
-    origin: 'https://mindfulfullstack-e1moh4oz5-chetangupta12344-gmailcom.vercel.app/', 
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
     optionsSuccessStatus: 204, 
